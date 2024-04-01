ea2d8660ec7682ef4d64e29f349f8f9e46371d67
md.obidulhuqsagor <md.obidulhuqsagor@localhost>
2024-04-01 22:26:04 +0600

    commit By Sagor

---
+++ NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/Mango_Garden_Enter_Page/LoginController.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package Mango_Garden_Enter_Page;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author md.obidulhuqsagor
+ */
+public class LoginController implements Initializable {
+
+    @FXML
+    private TextField nameTextfield;
+    @FXML
+    private TextField idTextfield;
+    @FXML
+    private TextField passwordTextfield;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void loginOnclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void createaccOnclick(ActionEvent event) {
+    }
+
+    @FXML
+    private void loginSupplychainManagerOnClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void loginHorticulturistOnClick(ActionEvent event) {
+    }
+    
+}
---
+++ NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/Mango_Garden_Enter_Page/mango_Garden_System_Software.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Mango_Garden_Enter_Page;
+
+/**
+ *
+ * @author md.obidulhuqsagor
+ */
+public class mango_Garden_System_Software {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
