5ecd9525a17ba0007cadc1d8e2d6ec55c0211e52
md.obidulhuqsagor <md.obidulhuqsagor@localhost>
2024-04-01 21:14:20 +0600

    commit By Sagor

--- NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/horticulturist_Sagor/horticulturist_Dashboard.java
+++ NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/horticulturist_Sagor/horticulturist_Dashboard.java
@@ -4,11 +4,26 @@
  */
 package horticulturist_Sagor;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  *
  * @author md.obidulhuqsagor
  */
-public class horticulturist_Dashboard {
+public class horticulturist_Dashboard extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("mainHorticulturist.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 
     /**
      * @param args the command line arguments
