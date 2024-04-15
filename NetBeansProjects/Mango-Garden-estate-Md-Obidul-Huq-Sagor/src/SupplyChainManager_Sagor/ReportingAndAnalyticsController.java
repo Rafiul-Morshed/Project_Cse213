3e2ce4d70be9cb890b00da38a9423ecdc7ac58d1
md.obidulhuqsagor <md.obidulhuqsagor@localhost>
2024-04-15 23:32:01 +0600

    commit By Sagor

--- NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/SupplyChainManager_Sagor/ReportingAndAnalyticsController.java
+++ NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/SupplyChainManager_Sagor/ReportingAndAnalyticsController.java
@@ -4,9 +4,15 @@
  */
 package SupplyChainManager_Sagor;
 
+import java.io.EOFException;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +21,8 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 /**
@@ -26,27 +33,67 @@ import javafx.stage.Stage;
 public class ReportingAndAnalyticsController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> raaorderidfxml;
+    private TableView<TransferBinFile> tabelviewraafxid;
     @FXML
-    private TableColumn<?, ?> raapurposefxml;
+    private TableColumn<TransferBinFile, String> raaNamefxid;
     @FXML
-    private TableColumn<?, ?> raaTaskfxml;
+    private TableColumn<TransferBinFile, String> raaOrderfxid;
     @FXML
-    private TableColumn<?, ?> raaResultsfxml;
+    private TableColumn<TransferBinFile, String> raaFromfxid;
     @FXML
-    private TableColumn<?, ?> raaValuefxml;
+    private TableColumn<TransferBinFile, String> raaTofxid;
     @FXML
-    private TableColumn<?, ?> raaamountfxml;
+    private TableColumn<TransferBinFile, String> raaAmountfxid;
     @FXML
-    private TextField raaOrderDdOnClick;
-
-    /**
-     * Initializes the controller class.
-     */
+    private TableColumn<TransferBinFile, String> raaMoveDafxid;
+    
+    
+    ArrayList<TransferBinFile> TransferFileList;
+    ObservableList<TransferBinFile> tempList = FXCollections.observableArrayList();
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        TransferFileList = new ArrayList <TransferBinFile>();
+        
+        raaNamefxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("Name"));
+        raaOrderfxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("From"));
+        raaFromfxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("TO"));
+        raaTofxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("OrderID"));
+        raaAmountfxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("Amount"));
+        raaMoveDafxid.setCellValueFactory(new PropertyValueFactory<TransferBinFile, String>("Amount"));
+        
+    }
+    private void loadTransferFile(){
+        ObjectInputStream ois = null;
+        try{
+            TransferBinFile i;
+            ois = new ObjectInputStream(new FileInputStream("Transfer.bin"));
+            while(true){
+                try {
+                    TransferBinFile ms = (TransferBinFile) ois.readObject();
+                    tempList.add(ms);
+                } catch (EOFException e) {
+                    break;
+                } catch (ClassNotFoundException e) {
+                    System.err.println("Class not Found");
+                }
+            }
+            for(TransferBinFile y : tempList){
+                 tabelviewraafxid.getItems().add(y);
+             }
+        }
+        catch(Exception e){
+            try{
+                if(ois != null)
+                    ois.close();
+            }
+            catch(IOException x){
+                x.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+    }
+        
 
     @FXML
     private void raabackOnClick(ActionEvent event) throws IOException {
@@ -58,7 +105,27 @@ public class ReportingAndAnalyticsController implements Initializable {
     }
 
     @FXML
-    private void raaSearchOnClick(ActionEvent event) {
+    private void raaShowReport(ActionEvent event) {
+                    ObjectInputStream ois = null;
+        try{
+            TransferBinFile i;
+            ois = new ObjectInputStream(new FileInputStream("Transfer.bin"));
+            while(true){
+                i = (TransferBinFile) ois.readObject();
+                tabelviewraafxid.getItems().add(i);
+            }
+        }
+        catch(Exception e){
+            try{
+                if(ois != null)
+                    ois.close();
+            }
+            catch(IOException x){
+                x.printStackTrace();
+            }
+            e.printStackTrace();
+        }
     }
+    
     
 }
--- NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/SupplyChainManager_Sagor/TransferManagementController.java
+++ NetBeansProjects/Mango-Garden-estate-Md-Obidul-Huq-Sagor/src/SupplyChainManager_Sagor/TransferManagementController.java
@@ -4,9 +4,14 @@
  */
 package SupplyChainManager_Sagor;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,8 +20,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 /**
@@ -29,17 +33,17 @@ public class TransferManagementController implements Initializable {
     @FXML
     private Button backTmfxid;
     @FXML
-    private TableView<?> tabelviewTmfxid;
+    private TextField tmNamefxid;
     @FXML
-    private TableColumn<?, ?> tmNamefxid;
+    private TextField tmFromfxid;
     @FXML
-    private TableColumn<?, ?> tmOrderfxid;
+    private TextField tmTofxid;
     @FXML
-    private TableColumn<?, ?> tmFromfxid;
+    private TextField tmOrderidfxid;
     @FXML
-    private TableColumn<?, ?> tmTofxid;
+    private TextField tmAmountfxid;
     @FXML
-    private TableColumn<?, ?> tmMovedatefxid;
+    private TextField tmMoveDatefxid;
 
     /**
      * Initializes the controller class.
@@ -57,5 +61,52 @@ public class TransferManagementController implements Initializable {
         window.setScene(scene1);
         window.show();
     }
+
+    @FXML
+    private void tmSubmitOnClick(ActionEvent event)throws IOException {
+
+        File f = null;
+        FileOutputStream fos = null;
+        ObjectOutputStream oos = null;
+        try{
+            f = new File("Transfer.bin");
+            if(f.exists()){
+                fos = new FileOutputStream(f, true);
+                oos = new AppendableObjectOutputStream(fos);
+                
+            }
+            else{
+                fos = new FileOutputStream(f);
+                oos = new ObjectOutputStream(fos);
+            }
+
+            TransferBinFile d = new TransferBinFile(tmNamefxid.getText(), tmFromfxid.getText(),
+                    tmTofxid.getText(), tmOrderidfxid.getText(),tmAmountfxid.getText(), 
+                    tmMoveDatefxid.getText());
+                    
+            oos.writeObject(d);
+
+            tmNamefxid.clear(); tmFromfxid.clear(); tmTofxid.clear();
+            tmOrderidfxid.clear(); tmAmountfxid.clear();tmMoveDatefxid.clear();
+            
+            fos.close();
+            oos.close();
+            
+        }
+        catch(IOException e){
+            Logger.getLogger(OrderManagementController.class.getName()).log(Level.SEVERE, null, e);
+    
+        }
+        finally{
+            try{
+                if(oos != null) oos.close();
+            }
+            catch(IOException e){
+                Logger.getLogger(OrderManagementController.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    
+    }
+    
     
 }
